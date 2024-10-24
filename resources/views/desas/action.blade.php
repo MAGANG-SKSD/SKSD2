d14d23c375b6b643983c99a3291b9ca9dceaac65
Ehaqq <a.adamezra@gmail.com>
2024-10-24 13:51:03 +0700

    Add nav-bar from nav-builder into feature profile desa

--- resources/views/desas/action.blade.php
+++ resources/views/desas/action.blade.php
@@ -4,18 +4,18 @@
     </button>
 
     <div class="dropdown-menu" x-placement="bottom-start">
-        <!-- Ganti $desa->id dengan $desa->desa_id -->
+        <!-- Edit action -->
         <a href="{{ route('desas.edit', $desa->desa_id) }}" class="dropdown-item">
             <i class="ti ti-edit action-btn"></i>{{ __('Edit') }}
         </a>
 
-        <!-- Ganti $desa->id dengan $desa->desa_id -->
+        <!-- Delete action -->
         <a href="{{ route('desas.index') }}" class="dropdown-item text-danger" data-toggle="tooltip"
             data-original-title="{{ __('Delete') }}" onclick="delete_record('delete-form-{{ $desa->desa_id }}')">
             <i class="ti ti-trash action-btn"></i>{{ __('Delete') }}
         </a>
 
-        <!-- Ganti $desa->id dengan $desa->desa_id -->
+        <!-- Form for deletion -->
         {!! Form::open(['method' => 'DELETE', 'route' => ['desas.destroy', $desa->desa_id], 'id' => 'delete-form-' . $desa->desa_id]) !!}
         {!! Form::close() !!}
     </div>
